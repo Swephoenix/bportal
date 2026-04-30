704c6c2532c3a5cbd5595e0abfad4a8e51a97a62
Swephoenix <andreas.jansson.2010@gmail.com>
2026-05-01 01:07:26 +0200

    Embed splash animation on login

--- server.js
+++ server.js
@@ -544,7 +544,7 @@ function sendText(res, statusCode, text, contentType = 'text/plain; charset=utf-
 }
 
 function serveStatic(req, res, pathname) {
-  const allowedPublic = new Set(['/', '/index.html', '/logo.png', '/paragraftecken.svg', '/socmedialinelogos.svg']);
+  const allowedPublic = new Set(['/', '/index.html', '/logo.png', '/paragraftecken.svg', '/socmedialinelogos.svg', '/Splash.html']);
   if (!allowedPublic.has(pathname)) {
     sendText(res, 404, 'Not found');
     return;
